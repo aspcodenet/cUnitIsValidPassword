a067b872bc24918e4ca42df89983b7c2839c3157
aspcodenet <stefan.holmberg@systementor.se>
2023-12-05 10:43:29 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "password.h"
+
+
+
+int main(){
+    char buffer[255];
+    while(1){
+        printf("The password tester\nEnter password:");
+        fgets(buffer, sizeof(buffer), stdin);
+        PASSWORD_ERROR error = checkPassword(buffer);
+        switch(error){
+            case PASSWORD_ERROR_OK:
+                printf("OK\n");
+                break;
+            case PASSWORD_ERROR_TOO_SHORT:
+                printf("Too short\n");
+                break;
+            case PASSWORD_ERROR_NO_UPPERCASE_LETTER:
+                printf("No uppercase\n");
+                break;
+            case PASSWORD_ERROR_NO_LOWERCASE_LETTER:
+                printf("No lowercase\n");
+                break;
+            case PASSWORD_ERROR_NO_NUMBER:
+                printf("No number\n");
+                break;
+            case PASSWORD_ERROR_CANT_CONTAIN_CERTAIN_WORDS:
+                printf("Cant contain certain words such as ....\n");
+                break;
+        }
+    }
+    return 0;
+}
---
+++ password.c
@@ -0,0 +1,31 @@
+#include "password.h"
+#include <string.h>
+#include <ctype.h>
+
+PASSWORD_ERROR checkPassword(const char *password){
+    if(strlen(password) < 8) return PASSWORD_ERROR_TOO_SHORT;
+
+    int uppercaseCount = 0;
+    int lowercaseCount = 0;
+    int digitCount = 0;
+    for(int i = 0; i < strlen(password); i++){
+        if(isupper(password[i]))
+            uppercaseCount++;
+        if(islower(password[i]))
+            lowercaseCount++;
+        if(isdigit(password[i]))
+            digitCount++;
+    }
+    if(uppercaseCount == 0) return PASSWORD_ERROR_NO_UPPERCASE_LETTER;
+    if(lowercaseCount == 0) return PASSWORD_ERROR_NO_LOWERCASE_LETTER;
+    if(digitCount == 0) return PASSWORD_ERROR_NO_NUMBER;
+
+
+    if(strstr(password,"password")) return PASSWORD_ERROR_CANT_CONTAIN_CERTAIN_WORDS;
+    if(strstr(password,"secret")) return PASSWORD_ERROR_CANT_CONTAIN_CERTAIN_WORDS;
+    if(strstr(password,"summer")) return PASSWORD_ERROR_CANT_CONTAIN_CERTAIN_WORDS;
+    if(strstr(password,"winter")) return PASSWORD_ERROR_CANT_CONTAIN_CERTAIN_WORDS;
+
+
+    return  PASSWORD_ERROR_OK;
+}   
